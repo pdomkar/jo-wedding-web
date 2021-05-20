09098dba886f7e1be2da6b07079a668f15c33a29
Petr Domkař <petr.domkar@gmail.com>
2021-05-20 22:32:44 +0200

    feat(): add block of page and add mobile version - unfinished

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,1 +1,16 @@
 console.log('%cTODO. 🍻', 'font-size: 20px; font-weight: bold')
+
+$(window).on('load', function () {
+    function fadeIn() {
+        var windowBottom = $(window).scrollTop() + $(window).innerHeight()
+
+        $('.fadeIn').each(function () {
+            var objectBottom = $(this).offset().top
+            /* If the begin of element is within bounds of the window, fade it in */
+            if (objectBottom + 100 < windowBottom) { // object comes into view (scrolling down)
+                $(this).fadeTo(1000, 1)
+            }
+        })
+    }
+    $(window).scroll(function () { fadeIn() }) // fade elements on scroll
+})
